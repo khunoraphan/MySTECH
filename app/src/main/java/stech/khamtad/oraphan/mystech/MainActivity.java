5d90991856ac3845d871028df35db3b3eb8b2305
oraphan <khunoraphan@hotmail.com>
2017-04-04 15:57:22 +0700

    InitialView OK

--- app/src/main/java/stech/khamtad/oraphan/mystech/MainActivity.java
+++ app/src/main/java/stech/khamtad/oraphan/mystech/MainActivity.java
@@ -2,12 +2,35 @@ package stech.khamtad.oraphan.mystech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View คือ การผูกตัวแปร กับ View ที่อยู่ ที่ Activity
+        initialView()
+
+
+    }   //Main Method
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView= (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+
     }
-}
+
+}   //Main Class นี่คือ คลาสหลัก
