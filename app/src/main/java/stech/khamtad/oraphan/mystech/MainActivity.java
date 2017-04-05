a4296e451228d6a0109f51dc2b7d296d0fa2e17d
oraphan <khunoraphan@hotmail.com>
2017-04-05 10:40:16 +0700

    Get Event From Click OK

--- app/src/main/java/stech/khamtad/oraphan/mystech/MainActivity.java
+++ app/src/main/java/stech/khamtad/oraphan/mystech/MainActivity.java
@@ -2,11 +2,13 @@ package stech.khamtad.oraphan.mystech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //Explicit
     private EditText userEditText, passwordEditText;
     private TextView textView;
@@ -19,10 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Initial View คือ การผูกตัวแปร กับ View ที่อยู่ ที่ Activity
-        initialView()
+        initialView();
+
+                //Create Controller
+        controller();
+
 
 
     }   //Main Method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
+    }
 
     private void initialView() {
         userEditText = (EditText) findViewById(R.id.edtUser);
@@ -33,4 +44,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-}   //Main Class นี่คือ คลาสหลัก
+    @Override
+    public void onClick(View v) {
+
+
+        String tag = "SriwattanaV1";
+
+        //For TextView
+        if (v == textView) {
+            Log.d(tag, "You Click TextView");
+        }
+        //For Button
+        if (v == button) {
+            Log.d(tag, "You Click Button");
+        }
+        }
+
+
+     //onClick
+
+} //Main Class นี่คือ คลาสหลัก
